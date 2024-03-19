ee7ebf4c278696e7a9fb180b007c755bf99924e8
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-03-19 09:12:01 +0530

    Added the hero Section

---
+++ src/components/CompareSlider.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
+
+const CompareSlider = () => {
+  return (
+    <ReactCompareSlider
+      itemOne={<ReactCompareSliderImage src="/input.jpg" />}
+      itemTwo={<ReactCompareSliderImage src="/output.png" />}
+    />
+  );
+};
+
+export default CompareSlider;
